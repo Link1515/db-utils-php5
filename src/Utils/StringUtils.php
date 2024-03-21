34b8829b310c43ec5922541a89762625420b2be2
Link1515 <h26281660@gmail.com>
2024-03-21 16:17:01 +0800

    add StringUtils

---
+++ src/Utils/StringUtils.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Link1515\DbUtilsPhp5\Utils;
+
+class StringUtils
+{
+  /**
+   * @param string $input
+   * @return string
+   */
+  public static function camelToSnake($input)
+  {
+    return strtolower(preg_replace('/([a-z0-9])([A-Z])/', '$1_$2', $input));
+  }
+
+  /**
+   * @param string $input
+   * @return string
+   */
+  public static function spaceAround($input)
+  {
+    return " " . $input . " ";
+  }
+}
